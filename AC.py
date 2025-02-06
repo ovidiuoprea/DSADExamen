636f69f7eeff1db4dbcda7b3707a84c75f27d5ec
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-02-06 23:32:41 +0200

    Canonica done

--- AC.py
+++ AC.py
@@ -62,5 +62,46 @@ corelatii = np.diag(np.corrcoef(z, u, rowvar=False)[:m, m:])
 c2 = corelatii * corelatii
 
 p_values = test_bartlett(c2, n, p, q, m)
-print(p_values)
+# print(p_values)
 
+etichete_radacini = ["root" + str(i+1) for i in range(m)]
+df_semnificatie = pd.DataFrame(
+    data={
+        "Corelatii": corelatii,
+        "Corelatii2": c2,
+        "P-values": p_values
+    }, index=etichete_radacini
+)
+df_semnificatie.to_csv("data_out/AC/root.csv")
+
+# Numar radacini semnificative
+numar_radacini_semnificative = np.where(p_values > 0.01)[0][0]
+
+numar_radacini_semnificative = numar_radacini_semnificative + 1 if numar_radacini_semnificative == 1 else numar_radacini_semnificative
+print("Radacini semnificative:", numar_radacini_semnificative)
+
+# Analiza legatura:
+corelatie_xz = np.corrcoef(x, z, rowvar=False)[:p, p:]
+corelatie_yu = np.corrcoef(y, u, rowvar=False)[:q, q:]
+
+df_corelatie_xz = pd.DataFrame(corelatie_xz, variabile_set1, etichete_z)
+df_corelatie_yu = pd.DataFrame(corelatie_yu, variabile_set2, etichete_u)
+
+df_corelatie_xz.to_csv("data_out/AC/xz.csv")
+df_corelatie_yu.to_csv("data_out/AC/yu.csv")
+
+# Varianta comuna:
+vx = np.sum(corelatie_xz[:, :m] * corelatie_xz[:, :m], axis=0)
+vy = np.sum(corelatie_yu[:, :m] * corelatie_yu[:, :m], axis=0)
+
+# Varianta redundanta:
+sx = vx * c2[:m]
+sy = vy * c2[:m]
+
+# DataFrame varianta / redundanta:
+df_vr = pd.DataFrame(data ={
+    'vx': vx, 'vy': vy, 'vx%': vx * 100 / p, 'vy%': vy * 100 / q,
+    'sx': sx, 'sy': sy, 'sx%': sx * 100 / p, 'sy%': sy * 100 / q
+}, index=etichete_radacini[:m])
+
+df_vr.to_csv("data_out/AC/varianta_redundanta.csv")
